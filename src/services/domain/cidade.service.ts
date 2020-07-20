e87f55663b02cca086e44285ea797293ac4315fd
Felipe Campanaro <felipe.s.campanaro@gmail.com>
2020-07-20 19:43:09 -0300

    Inserting state and city on signuppage

---
+++ src/services/domain/cidade.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { CidadeDTO } from "../../models/cidade.dto";
+import { Observable } from "rxjs/Rx";
+import { API_CONFIG } from "../../config/api.config";
+
+@Injectable()
+export class CidadeService {
+
+    constructor(public http: HttpClient){
+
+    }
+
+    findAll(estado_id) : Observable<CidadeDTO[]> {
+        return this.http.get<CidadeDTO[]>(`${API_CONFIG.baseUrl}/estados/${estado_id}/cidades`);
+    }
+}
